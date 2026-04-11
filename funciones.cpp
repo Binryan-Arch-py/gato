67b504e44f527b96cd219f5b72c17fd9fad575d8
Bryan <bryanarana2606@gmail.com>
2026-04-11 00:30:35 -0600

    refactor: mejorar lógica y rendimiento en comprobación de función

--- funciones.cpp
+++ funciones.cpp
@@ -31,8 +31,8 @@ void ver_tabla(const char (&tabla)[4][4]) {
 
 bool seguir_juego(const char (&tabla)[4][4]) {
     bool fin = true;
-    for (int i = 1; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 3; j++) {
             if (tabla[i][j] == '#') {
                 fin = false;
             }
